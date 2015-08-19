50f67b2598d068ecdd90395d47f00a179aef0559
Lynn Langit <lynnlangit@live.com>
2015-08-19 19:26:22 +0000

    added all problems

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ for l in letters:
   else:
   	 print(l)
 print("\n"+"This is puzzle #3")
+
+#4. STRING OPERATIONS Given a character array which contains a sentence 
+#(words separated by spaces), given the indices of two words in the character array, 
+#swap the words – words can be anywhere in the sentence, and can be of different lengths.
+
+#5. RECURUSION - NODE WALKING Given the root of a binary tree, where each node has 
+#an integer value and has left and right child pointers, print out the values of all the nodes 
+#in the tree, in level order. i.e, root first, then its children if any, then grandchildren 
+#if any (in visual order left to right), and so on.
+
+#6. RECURISON - NODE SEARCHING Given two nodes in a binary tree where each node structure 
+#has a left child, right child and parent pointer, find the node that is the closest common ancestor.
+
+#7. RECURSION - NODE OPERATIONS Given the root of a binary tree, 
+#where each node has a left pointer and a right pointer, mirror the tree – 
+#i.e., for every node in the tree, its former left child should become its right child 
+#and its former right child should become its left child.
