5e2a82c224fb04037831e7edcf96c8e90d255c4e
Lynn Langit <lynnlangit@live.com>
2015-08-19 00:56:15 +0000

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+#1. Print int array, neg, then zero, then positive
+numbers = [0,1,2,-1,0,3]
+numbers.sort()
+for num in numbers:
+	print(num)
+print("\n"+"This is puzzle #1")
+
+#2. Print all palindromes in string of letters
+letters = 'aaaabbbcccaaaz'
+for l in letters:
+  print(l)
+print("\n"+"This is puzzle #2")
